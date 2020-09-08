e1d2a15667c4c9b31f2e881127a0e74bc05f04f4
nqounet <mail@nqou.net>
2020-09-08 16:03:35 +0900

    done 2.5.2(master)

---
+++ chapter-2.5.1/main.kt
@@ -0,0 +1,31 @@
+import java.io.BufferedReader
+import java.io.StringReader
+
+fun readNumber(reader: BufferedReader): Int? {
+    try {
+        val line = reader.readLine()
+        return Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        return null
+    } finally {
+        reader.close()
+    }
+}
+
+fun main() {
+    val reader = BufferedReader(StringReader("239"))
+    println(readNumber(reader))
+
+    val readerK = BufferedReader(StringReader("not a number"))
+    readNumberKotlin(readerK)
+}
+
+fun readNumberKotlin(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        null
+    }
+
+    println(number)
+}
