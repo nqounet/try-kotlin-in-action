0a5a750699f65ce7b6eeb299e0f000bd3b44d74a
nqounet <mail@nqou.net>
2020-09-09 19:51:30 +0900

    done 3.3.4(master)

---
+++ chapter-3.2.3/join.kt
@@ -0,0 +1,5 @@
+package strings
+
+fun String.lastChar(): Char = this.get(this.length - 1)
+
+fun String.firstChar(): Char = get(0)
---
+++ chapter-3.2.3/main.kt
@@ -0,0 +1,7 @@
+import strings.firstChar
+import strings.lastChar
+
+fun main() {
+    println("Kotlin".lastChar())
+    println("Kotlin".firstChar())
+}
