13353cde09e0972b6ac2a33dadd7538bc7509c8f
nqounet <mail@nqou.net>
2020-09-20 08:26:36 +0900

    done 4.2.4(main)

---
+++ chapter-4.2.4/User.kt
@@ -0,0 +1,12 @@
+class User(val name: String) {
+    var address: String = "unspecified"
+        set(value: String) {
+            println(
+                """
+            Address was changed for $name:
+            "$field" -> "$value".
+                """.trimIndent()
+            )
+            field = value
+        }
+}
---
+++ chapter-4.2.4/main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val alice = User("Alice")
+    alice.address = "Elsenhimerstrasse 47, 80687 Muenchen"
+}
