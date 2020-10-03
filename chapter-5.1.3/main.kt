edc1b8ecec042d6a4a1758d98bb9880093265738
nqounet <mail@nqou.net>
2020-10-04 07:01:21 +0900

    add コメント(main)

--- chapter-5.1.3/main.kt
+++ chapter-5.1.3/main.kt
@@ -11,11 +11,11 @@ fun main() {
     println(sum2(2, 3))
 
     val people = listOf(Person("Alice", 29), Person("Bob", 32))
-    println(people.maxByOrNull({ p: Person -> p.age }))
-    println(people.maxByOrNull() { p: Person -> p.age })
-    println(people.maxByOrNull { p: Person -> p.age })
-    println(people.maxByOrNull { p -> p.age })
-    println(people.maxByOrNull { it.age })
+    println(people.maxByOrNull({ p: Person -> p.age })) // 通常の文法（maxByOrNullの引数にラムダを渡す）
+    println(people.maxByOrNull() { p: Person -> p.age }) // 最後の引数がラムダの場合、カッコの外に書ける
+    println(people.maxByOrNull { p: Person -> p.age }) // ラムダが唯一の引数の場合は、カッコを省略できる
+    println(people.maxByOrNull { p -> p.age }) // 型が自明の場合は型推論が可能
+    println(people.maxByOrNull { it.age }) // 引数が一つの場合は、itが生成される
 
     val getAge = { p: Person -> p.age }
     println(people.maxByOrNull(getAge))
