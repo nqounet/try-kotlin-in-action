f43d16919907146dd100d10da99930f071ca5e91
nqounet <mail@nqou.net>
2020-10-03 23:18:16 +0900

    done 5.1.2(main)

---
+++ chapter-5.1.1/main.kt
@@ -0,0 +1,10 @@
+/* Java */
+button.setOnClickListener(new OnClickListener(){
+    @Override
+    public void onClick(View view){
+        /* actions on click */
+    }
+});
+
+/* Kotlin */
+button.setOnClickListener {/* actions on click */}
---
+++ chapter-5.1.2/main.kt
@@ -0,0 +1,19 @@
+fun findOldest(people: List<Person>) {
+    var maxAge = 0
+    var theOldest: Person? = null
+    for (person in people) {
+        if (person.age > maxAge) {
+            maxAge = person.age
+            theOldest = person
+        }
+    }
+    println(theOldest)
+}
+
+fun main() {
+    val people = listOf(Person("Alice", 29), Person("Bob", 32))
+    findOldest(people)
+
+    println(people.maxByOrNull { it.age })
+    println(people.minByOrNull(Person::age))
+}
