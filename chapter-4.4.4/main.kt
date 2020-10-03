d31b2368ab38f827c4f27584b7457a7976dac02c
nqounet <mail@nqou.net>
2020-10-03 17:25:09 +0900

    done 4.4.4(main)

---
+++ chapter-4.4.4/main.kt
@@ -0,0 +1,18 @@
+
+/*
+// TODO: 動かし方が不明
+import java.awt.event.MouseAdapter
+
+window.addMouseListener(
+    // 無名オブジェクトの宣言として利用できる
+    object: MouseAdapter(){
+        override fun mouseClicked(e: MouseEvent){
+            TODO("todo")
+        }
+
+        override fun mouseEntered(e: MouseEvent){
+            TODO("todo")
+        }
+    }
+)
+ */
